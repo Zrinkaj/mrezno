8e3b2d639fd04df3b26294386c11dbbc5ca2b27e
Zrinkaj <78958362+Zrinkaj@users.noreply.github.com>
2021-05-31 23:00:43 +0200

    Dodajem vjezbu 9

---
+++ LABno9/client2.py
@@ -0,0 +1,32 @@
+import socket
+import ssl
+import datetime
+
+from server_ssl import HOST as SERVER_HOST
+from server_ssl import PORT as SERVER_PORT
+
+print("Program se izvodi na ovom racunalu:")
+print(datetime.datetime.now())
+
+from local_machine_info import print_machine_info
+
+print_machine_info()
+print("----------------------------------------------")
+
+HOST = "127.0.0.1"
+PORT = 60002
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+client = ssl.wrap_socket(client, keyfile="key.pem", certfile="cert.pem")
+
+if __name__ == "__main__":
+    client.bind((HOST, PORT))
+    client.connect((SERVER_HOST, SERVER_PORT))
+
+    while True:
+        from time import sleep
+
+        client.send("Hello World!".encode("utf-8"))
+        sleep(1)
